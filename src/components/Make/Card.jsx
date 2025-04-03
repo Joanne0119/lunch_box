759ee561a603cfd2eb04b222e8f2f17f407c77d7
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2025-04-03 14:44:22 +0800

    add some image for order items

--- src/components/Make/Card.jsx
+++ src/components/Make/Card.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Card = ({ item, isSelected, onSelect }) => {
   return (
     <div 
-      className={`card bg-base-200 m-4 shadow-sm cursor-pointer ${isSelected ? "border-4 border-base-primary" : ""}`}
+      className={`card bg-base-200 m-4 shadow-sm cursor-pointer transition-all duration-300 ${isSelected ? "border-4 border-base-primary" : ""}`}
       onClick={onSelect}
     >
-      <figure>
-        <img src={item.image} alt={item.name} />
+      <figure className='p-2'>
+        <img className="p-4 rounded-lg bg-base-100 inset-shadow-sm" src={item.image} alt={item.name} />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{item.name}</h2>
-        <p>{item.description}</p>
+        <p>{item.calories} kcal</p>
       </div>
     </div>
   )
--- src/components/Make/ConfirmOrder.jsx
+++ src/components/Make/ConfirmOrder.jsx
@@ -10,7 +10,7 @@ const ConfirmOrder = ({ selectedIngredients }) => {
           .sort((a, b) => Number(a.replace("step", "")) - Number(b.replace("step", "")))
           .map((step) => (
             selectedIngredients[step].length > 0 && (
-              <div key={step} className="mt-2 p-4 border border-gray-300 rounded-lg">
+              <div key={step} className="mt-2 p-4 border border-primary rounded-lg">
                 <h2 className="text-lg font-semibold">{ orderHints[step.slice(-1) - 1].slice(4)}</h2>
                 <ul>
                   {selectedIngredients[step].map((item) => (
